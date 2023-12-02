ee219a19357cb5c18953cfa99139dd30a84c7976
Jin Kwon <onacit@gmail.com>
2023-12-02 20:38:21 +0900

    wip

--- src/main/java/com/github/jinahya/example/Application.java
+++ src/main/java/com/github/jinahya/example/Application.java
@@ -1,9 +1,13 @@
 package com.github.jinahya.example;
 
 import com.github.jinahya.juso.go.kr.api.proxy._NoOp;
+import com.github.jinahya.juso.go.kr.api.proxy.stereotype.AddrRetrievalEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
 
 @ConfigurationPropertiesScan(
         basePackageClasses = {
@@ -16,9 +20,15 @@ import org.springframework.context.annotation.ComponentScan;
         }
 )
 @SpringBootApplication
+@Slf4j
 class Application {
 
     public static void main(final String... args) {
-        org.springframework.boot.SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
+    }
+
+    @EventListener
+    void retrieved(final AddrRetrievalEvent<?, ?> event) {
+        log.debug("retrieved: {}", event);
     }
 }
