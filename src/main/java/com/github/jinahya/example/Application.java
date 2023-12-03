a118ac8c78737ed2e5bbe48d08afd38fe068bab2
Jin Kwon <onacit@gmail.com>
2023-12-03 20:28:12 +0900

    wip

--- src/main/java/com/github/jinahya/example/Application.java
+++ src/main/java/com/github/jinahya/example/Application.java
@@ -1,14 +1,22 @@
 package com.github.jinahya.example;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.jinahya.juso.go.kr.api.proxy._NoOp;
 import com.github.jinahya.juso.go.kr.api.proxy.stereotype.AddrRetrievalEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
 
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
 @ConfigurationPropertiesScan(
         basePackageClasses = {
                 _NoOp.class
@@ -25,6 +33,14 @@ class Application {
 
     public static void main(final String... args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CacheManager cacheManager() {
+        final var cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES));
+        cacheManager.setAsyncCacheMode(true);
+        return cacheManager;
     }
 
     @EventListener
